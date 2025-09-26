7d7994b5a74aea8357e7a9790b72e2563e85c032
Yanis <yanis.et.co04@gmail.com>
2025-09-26 15:44:04 +0200

    Initialisation du nouveau projet

---
+++ PartiKar/src/test/java/com/example/PartiKar/PartiKarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PartiKar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartiKarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
