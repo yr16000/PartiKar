944a03b746986f34aa01b833352f9bebc1601059
Yanis <yanis.et.co04@gmail.com>
2025-10-29 12:48:23 +0100

    ajout de toutes les tables

---
+++ backend/src/main/java/com/partikar/voiture/VoitureRepository.java
@@ -0,0 +1,14 @@
+package com.partikar.voiture; // Assure-toi que le package est correct
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository // Indique à Spring que c'est un composant Repository
+public interface VoitureRepository extends JpaRepository<Voiture, Long> {
+    List<Voiture> findByProprietaireId(Long proprietaireId);
+    List<Voiture> findByMarqueContainingIgnoreCase(String marque);
+    List<Voiture> findByLocalisationContainingIgnoreCase(String ville);
+
+}
